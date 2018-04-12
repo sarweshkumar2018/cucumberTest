003c976014bae650eb19dda5ed5449802fb3d9a9
sarwe <sarwe@DESKTOP-JGRVJ6V.attlocal.net>
2018-04-11 21:16:29 -0500

    phantomjs update

--- src/test/java/com/citi/cucumberTest/stepDefinitions/stepDefinition.java
+++ src/test/java/com/citi/cucumberTest/stepDefinitions/stepDefinition.java
@@ -2,28 +2,28 @@ package com.citi.cucumberTest.stepDefinitions;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-
-import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class stepDefinition {
-	//WebDriver dr;
-	HtmlUnitDriver dr;
-	
+	WebDriver dr;
+
 	@Given("^User is on (.*) welcome screen$")
 	public void welcomescreen(String expectedTitle) throws InterruptedException {
 		openBrowser();
-		Thread.sleep(5000);
-		
+		// Thread.sleep(5000);
+
 		String title = dr
 				.findElement(By
 						.xpath("/html/body/app-root/cbol-core/citi-parent-layout/div/div/div/app-fixed-payment/citi-simple-layout/app-landing/citi-hero/section/div/div[2]/div/div/div/div/div/div/div/citi-text-header/h1"))
 				.getText();
-		
+
 		Assert.assertTrue(expectedTitle.equals(title));
 	}
 
@@ -34,24 +34,24 @@ public class stepDefinition {
 
 	@Then("^User should navigate to (.*) screen$")
 	public void verifySuccessful(String expectedTitle) throws InterruptedException {
-		Thread.sleep(5000);
+		// Thread.sleep(5000);
 		String title = dr
 				.findElement(By
 						.xpath("/html/body/app-root/cbol-core/citi-parent-layout/div/div/div/app-fixed-payment/citi-simple-layout/app-customize-loan/section/div/citi-text-header/h1"))
 				.getText();
 		Assert.assertTrue(expectedTitle.equals(title));
-		// closeBrowser();
+
 	}
 
 	@Given("^User is navigated to (.*) screen$")
 	public void verifyScreen(String expectedTitle) throws InterruptedException {
-		//Thread.sleep(5000);
+		// Thread.sleep(5000);
 		System.out.println(dr
 				.findElement(By
 						.xpath("/html/body/app-root/cbol-core/citi-parent-layout/div/div/div/app-fixed-payment/citi-simple-layout/app-customize-loan/section/div/citi-text-header/h1"))
 				.getText());
 		String title = dr
-				.findElement(By 
+				.findElement(By
 						.xpath("/html/body/app-root/cbol-core/citi-parent-layout/div/div/div/app-fixed-payment/citi-simple-layout/app-customize-loan/section/div/citi-text-header/h1"))
 				.getText();
 		Assert.assertTrue(expectedTitle.equals(title));
@@ -60,27 +60,35 @@ public class stepDefinition {
 	@When("^User enters loan amount$")
 	public void enterAmount() {
 		dr.findElement(By.xpath("//*[@id=\"loanAmountInput\"]")).clear();
-		dr.findElement(By.xpath("//*[@id=\"loanAmountInput\"]")).sendKeys("2000");
+		dr.findElement(By.xpath("//*[@id=\"loanAmountInput\"]")).sendKeys("3");
 	}
 
 	@Then("^Entered amount is shown in the loan amount field$")
 	public void verifyAmount() {
-        String amount = dr
+		String amount = dr
 				.findElement(By
 						.xpath("/html/body/app-root/cbol-core/citi-parent-layout/div/div/div/app-fixed-payment/citi-simple-layout/app-customize-loan/section/citi-form-container/form/div/div[1]/citi-row/div/citi-column[2]/div/citi-row[1]/div/p"))
 				.getText();
-		Assert.assertTrue(amount.equals("$2,000"));
-		//closeBrowser();
+		Assert.assertTrue(amount.equals("$3"));
+		closeBrowser();
 	}
 
 	public void openBrowser() {
-		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");
-		//dr = new FirefoxDriver();
-		//dr = new ChromeDriver();
-		dr = new HtmlUnitDriver(BrowserVersion.CHROME);
+		// System.setProperty("webdriver.chrome.driver",
+		// "C:\\selenium\\chromedriver.exe");
+		// System.setProperty("webdriver.gecko.driver",
+		// "C:\\selenium\\geckodriver.exe");
+		// System.setProperty("webdriver.phantomjs.driver",
+		// "C:\\selenium\\phantomjs.exe");
+		// dr = new FirefoxDriver();
+		// dr = new ChromeDriver();
+		// dr = new HtmlUnitDriver();
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setJavascriptEnabled(true);
+		cap.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "C:/selenium/phantomjs.exe");
+		dr = new PhantomJSDriver(cap);
 		dr.get("https://dit57.online.citi.com/US/ag/loan-on-card/landing");
-		
+
 	}
 
 	public void closeBrowser() {
